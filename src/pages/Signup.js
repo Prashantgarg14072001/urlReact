185891b48a0bdfa30e8d27132e3aa276330ee1b6
Prashantgarg14072001 <114285431+Prashantgarg14072001@users.noreply.github.com>
2023-12-29 17:18:56 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './SignupLogin.css';
 
-const Signup = ({ onToggle }) => {
+const Signup = ({ onToggle, onSignupSuccess }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [signupSuccess, setSignupSuccess] = useState(false);
 
   const handleSignup = async () => {
     try {
@@ -20,6 +21,11 @@ const Signup = ({ onToggle }) => {
 
       if (response.ok) {
         console.log('Signup successful:', data);
+        setSignupSuccess(true);
+        alert('Signup successful! Please login.');
+        setUsername('');
+        setPassword('');
+        onSignupSuccess();
       } else {
         setError(data.message || 'Error signing up');
       }
@@ -31,24 +37,30 @@ const Signup = ({ onToggle }) => {
   return (
     <div className="signup-login-card">
       <h2>Signup</h2>
-      <div className="input-group">
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div className="input-group">
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <div className="button-group">
-        <button onClick={handleSignup}>Sign Up</button>
-        <p>
-          Already have an account?{' '}
-          <span className="toggle-link" onClick={() => onToggle('login')}>
-            Login
-          </span>
-        </p>
-      </div>
-      {error && <p className="error-message">{error}</p>}
+      {signupSuccess ? (
+        <p className="success-message">Signup successful! Please login.</p>
+      ) : (
+        <>
+          <div className="input-group">
+            <label>Username:</label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div className="input-group">
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div className="button-group">
+            <button onClick={handleSignup}>Sign Up</button>
+            <p>
+              Already have an account?{' '}
+              <span className="toggle-link" onClick={() => onToggle('login')}>
+                Login
+              </span>
+            </p>
+          </div>
+          {error && <p className="error-message">{error}</p>}
+        </>
+      )}
     </div>
   );
 };
@@ -74,6 +86,7 @@ const Login = ({ onToggle }) => {
         const { token, username } = data;
         localStorage.setItem('authToken', token);
         localStorage.setItem('userData', JSON.stringify({ username, token }));
+        alert('Login successful!');
         window.location.href = '/dashboard';
       } else {
         setError(data.message || 'Invalid credentials');
@@ -115,9 +128,17 @@ const SignupLogin = () => {
     setActiveForm(form);
   };
 
+  const handleSignupSuccess = () => {
+    setActiveForm('login');
+  };
+
   return (
     <div className="signup-login-container">
-      {activeForm === 'signup' ? <Signup onToggle={handleToggle} /> : <Login onToggle={handleToggle} />}
+      {activeForm === 'signup' ? (
+        <Signup onToggle={handleToggle} onSignupSuccess={handleSignupSuccess} />
+      ) : (
+        <Login onToggle={handleToggle} />
+      )}
     </div>
   );
 };
